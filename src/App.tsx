7ae913e645422c562ca2bb3b04e343f186f6d79c
Alex Lecco <alexlecco24@gmail.com>
2024-04-10 10:07:28 -0300

    implement UI for interchange languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Container, Row, Col, Button } from 'react-bootstrap'
+
 import './App.css'
 import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGE } from './constants'
+import { ArrowsIcon } from './components/Icons'
 
 function App() {
-  const { fromLanguage, setFromLanguage } = useStore()
+  const {
+    fromLanguage,
+    toLanguage,
+    setFromLanguage,
+    interchangeLanguages
+  } = useStore()
 
   return (
-    <>
+    <Container fluid>
       <h1>Google Translate</h1>
-      <button
-        onClick={() => {
-          setFromLanguage('es')
-        }}
-      >
-        Cambiar a Español
-      </button>
-      {fromLanguage}
-    </>
+
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button
+            variant='link'
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
