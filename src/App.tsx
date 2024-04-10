f89b7abed6d4b901ecf934e1a93ac70315585bfe
Alex Lecco <alexlecco24@gmail.com>
2024-04-10 08:46:36 -0300

    create useStore hook and export masked dispatch functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useReducer } from 'react'
 import './App.css'
-import { type State, type Action } from './types.d'
-
-// 1. Create a initial state
-const initialState: State = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false
-}
-
-// 2. Create a reducer
-function reducer(state: State, action: Action) {
-  const { type } = action
-
-  if (type === 'INTERCHANGE_LANGUAGES') {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    }
-  }
-
-  if (type === 'SET_FROM_LANGUAGE') {
-    return {
-      ...state,
-      fromLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_TO_LANGUAGE') {
-    return {
-      ...state,
-      toLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_FROM_TEXT') {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: ''
-    }
-  }
-
-  if (type === 'SET_RESULT') {
-    return {
-      ...state,
-      loading: false,
-      fromText: action.payload
-    }
-  }
-
-  return state
-}
+import { useStore } from './hooks/useStore'
 
 function App() {
-  // 3. usar el hook useReducer
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const { fromLanguage, setFromLanguage } = useStore()
 
   return (
     <>
       <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          setFromLanguage('es')
+        }}
+      >
+        Cambiar a Español
+      </button>
+      {fromLanguage}
     </>
   )
 }
---
+++ src/hooks/useStore.ts
@@ -0,0 +1,101 @@
+import { useReducer } from 'react'
+import { type State, type Action } from '../types.d'
+
+// 1. Create a initial state
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+// 2. Create a reducer
+function reducer(state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      fromText: action.payload
+    }
+  }
+
+  return state
+}
+
+export function useStore () {
+  // 3. usar el hook useReducer
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+  }, dispatch] = useReducer(reducer, initialState)
+
+  const interchangeLanguages = () => {
+    dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+  }
+
+  const setFromLanguage = (payload: string) => {
+    dispatch({ type: 'SET_FROM_LANGUAGE', payload })
+  }
+
+  const setToLanguage = (payload: string) => {
+    dispatch({ type: 'SET_TO_LANGUAGE', payload })
+  }
+
+  const setFromText = (payload: string) => {
+    dispatch({ type: 'SET_FROM_TEXT', payload })
+  }
+
+  const setResult = (payload: string) => {
+    dispatch({ type: 'SET_RESULT', payload })
+  }
+
+  return {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
+  }
+}
